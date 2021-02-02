1ffa0986d0d3e3e6f4a0b6a40386342b47649645
Nuttapong9911 <nuttapongboonsala@gmail.com>
2021-02-02 15:42:36 +0700

    completed

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -17,3 +17,50 @@ int main(){
 	
 	return 0;
 }
+
+
+void shuffle(int &a,int &b,int &c,int &d) {
+	int x = rand()%6;
+	int temp;
+
+	switch (x)
+	{
+	case (1):
+		temp = a;
+		a = b;
+		b = temp;
+		break;
+	
+	case (2):
+		temp = a;
+		a = c;
+		c = temp;
+		break;
+
+	case (3):
+		temp = a;
+		a = d;
+		d = temp;
+		break;
+
+	case (4):
+		temp = b;
+		b = c;
+		c = temp;
+		break;
+
+	case (5):
+		temp = b;
+		b = d;
+		d = temp;
+		break;
+
+	case (6):
+		temp = c;
+		c = d;
+		d = temp;
+		break;
+	
+	} 
+
+}
